677963c2e4c6bdfbccaee73471f9b5df2244c755
Kimberly <kmglock@mail.lipscomb.edu>
2021-01-03 15:37:43 -0600

    commit tree 3fd45cc83e23d7f38af06257ffe37a30655cd5b7

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+# anagram = a word, phrase, or sentence formed from another by rearranging its letters
+# exmp: top and pot and opt
+
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        array.select do |element|
+            (@word.split("").sort) == (element.split("").sort)
+        end
+    end
+end
